0845433da0b366003bdcc2cccab8d22f85978e08
Nate Solyntjes <njsoly@syntj.com>
2018-11-04 01:57:51 -0600

    renamed and moved SimplyPath.java

---
+++ cs/interviewpractice/SimplifyPath.java
@@ -0,0 +1,62 @@
+package cs.interviewpractice;
+
+/**
+ * SimplifyPath.java: 
+ * https://app.codesignal.com/interview-practice/task/aRwxhGcmvhf6vKPCp
+ */
+public class SimplifyPath {
+		
+	String simplifyPath(String path) {
+	
+	    // start with root if not yet
+	    if(path.charAt(0) != '/'){
+	        path = "/" + path;
+	    }
+	    
+	    int i = 0;
+	    while(path.contains("//")){
+	        path = path.substring(0,path.indexOf("//")+1) + path.substring(path.indexOf("//")+2);
+	
+	    }
+	    i = path.indexOf("/./");
+	    while(i != -1){
+	        path = path.substring(0,i) + "/" + path.substring(i+3);
+	        
+	        i = path.indexOf("/./");
+	    }
+	    
+	    i = path.indexOf("/..");
+	    while(i != -1){
+	        int up = i;
+	        while(--up >= 0){
+	            if(path.charAt(up) == '/'){
+	                break;
+	            }
+	        }
+	        if(up > 0){
+	            path = path.substring(0,up) + path.substring(i + 3);
+	        }
+	        else {
+	            path = path.substring(3);
+	        }
+	        
+	        
+	        i = path.indexOf("/..");
+	    }
+	   
+	    // remove leading dots 
+	    while(path.startsWith("../")){
+	        path = path.substring(2);
+	    }
+	    while(path.startsWith("./")){
+	        path = path.substring(1);
+	    }
+	    
+	    // make sure it doesn't end in '/' unless that's all it is
+	    if(path.length() > 1 && path.endsWith("/")){
+	        path = path.substring(0,path.length() - 1);
+	    }    
+	    return path;
+	}
+
+}
