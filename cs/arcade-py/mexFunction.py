f0424891a148ebc159c9c9ebcb729963cbe9e2e3
Nate Solyntjes <njsoly@syntj.com>
2018-11-12 23:35:33 -0600

    +mexFunction.py: a codeS1gn4l solution.

---
+++ cs/arcade-py/mexFunction.py
@@ -0,0 +1,16 @@
+#!/bin/python
+#
+# mexFunction.py
+# 
+# 
+
+def mexFunction(s, upperBound):
+    found = -1
+    for i in range(upperBound):
+        if not i in s:
+            found = i
+            break
+    else:
+        found=upperBound
+
+    return found
