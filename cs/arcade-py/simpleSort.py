1062b5cbcd21b0f56b7f21d0758851d0404955da
Nate Solyntjes <njsoly@syntj.com>
2019-01-05 09:22:41 -0600

    +simpleSort.py

---
+++ cs/arcade-py/simpleSort.py
@@ -0,0 +1,14 @@
+# https://app.codesignal.com/arcade/python-arcade/meet-python/MEgcxkQyYqFDdySnH/solutions?solutionId=Zz2itpkxJK6gSDZBq
+
+def simpleSort(arr):
+
+    n = len(arr)
+
+    for i in range(n):
+        j = 0
+        stop = n - i
+        while j < stop - 1:
+            if arr[j] > arr[j + 1]:
+                ... = ...
+            j += 1
+    return arr
